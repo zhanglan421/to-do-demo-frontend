cbf444120b5d17b8b0c378550b5c4bbd832303cb
zhanglan <874259005@qq.com>
2024-07-16 15:25:48 +0800

    Initialize project with Vite and Vue 3

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import home from '../views/home/index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    }
+  ]
+})
+
+export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col justify-center items-center h-screen w-screen">
+        <div class="text-2xl font-bold">你好，这是你的任务清单助手</div>
+
+        <div class="flex flex-col justify-center items-center mt-10 w-80 h-80 border-solid border border-black">
+            <input type="text" class=" outline-none border mb-3">
+
+            <input type="password" class=" outline-none border">
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        return {}
+    }
+})
+</script>
+
+<style lang="scss" scoped></style>
